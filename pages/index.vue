2a9fe5c991ad2ec6f95d85a8d69039fac0175c4e
Fabio <fabiorg.yx@gmail.com>
2023-03-22 12:05:07 -0300

    Projeto VUE

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p> Página Inicial </p>
+    <form @submit.prevent="receberDados">
+      <input v-model="inputNome" type="text" name="nome" id="nome">
+      <button> Enviar </button>
+    </form>
+    <ul>
+      <li v-for="name in names"> {{ name }} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      names: [],
+      inputNome: ""
+    }
+  },
+  methods: {
+    receberDados(){
+      console.log(this.inputNome)
+      this.names.push(this.inputNome)
+    }
+  }
+}
+</script>
